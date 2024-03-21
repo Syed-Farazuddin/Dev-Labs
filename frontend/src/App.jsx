1f5b0e41d5b227b5f905beabfd3ef2095e1d65a9
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-21 11:45:17 +0530

    register connectivity to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/UserPages/Login.jsx";
 import Register from "./pages/UserPages/Register.jsx";
 import Homepage from "./pages/Homepage.jsx";
-
+import AnimatedRegister from "./pages/UserPages/AnimatedRegister.jsx";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/register2" element={<AnimatedRegister />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/UserPages/Register.jsx
+++ frontend/src/pages/UserPages/Register.jsx
@@ -1,20 +1,41 @@
 import React, { useState } from "react";
 import logo from "../../assets/D.png";
-
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const navigate = useNavigate();
+  const handleSubmit = async () => {
+    const response = await axios.post("http://localhost:4000/register", {
+      email,
+      password,
+    });
+    console.log(response);
+    if (response.data.success) {
+      navigate("/login");
+    }
+  };
   return (
-    <div className="flex items-center justify-center h-[100vh] flex-col">
-      <h1 className="text-xl mb-4 font-bold flex justify-center items-end gap-2">
-        Sign up to{" "}
-        <div className="flex justify-center items-end ml-2">
-          <img className="w-10 h-10" src={logo} alt="" />
+    <div className="flex items-center justify-center h-[100vh] flex-col normalRegister">
+      <div className="flex fixed top-0 right-0 left-0  py-4 px-6 justify-between items-center">
+        <div className="flex items-center justify-end">
+          <img className="h-10 w-10" src={logo} alt="" />
           <p>ev Labs</p>
         </div>
+        <div className="flex text-white gap-4">
+          <p className="text-slate-400">Already have an Account ?</p>
+          <span className="hover:underline cursor-pointer">Sign in</span>
+        </div>
+      </div>
+      <h1 className="text-xl mb-4 font-bold flex justify-center items-end gap-2 text-white">
+        Welcome to
+        <div className="flex justify-center items-end ml-2">
+          <p className="text-white">Dev Labs</p>
+        </div>
       </h1>
-      <div className="p-10 flex flex-col  gap-5 bg-slate-200 rounded-lg">
+      <div className="p-10 flex flex-col  gap-5 bg-slate-500 rounded-lg">
+        <h1>Sign up</h1>
         <div className="flex flex-col gap-2">
           <label htmlFor="Email">Email address</label>
           <input
@@ -38,7 +59,12 @@ function Register() {
           />
         </div>
 
-        <button className="px-4 py-2 w-full bg-buttonBlue rounded-lg text-white">
+        <button
+          className="px-4 py-2 w-full bg-buttonBlue rounded-lg text-white"
+          onClick={() => {
+            handleSubmit();
+          }}
+        >
           Sign up
         </button>
       </div>
