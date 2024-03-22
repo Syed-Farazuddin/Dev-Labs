d65904f4063ca2a9871f6a837588698b8afae98e
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-22 11:46:30 +0530

    major validations added! User ID validation to be done

--- frontend/src/pages/UserPages/AnimatedRegister.jsx
+++ frontend/src/pages/UserPages/AnimatedRegister.jsx
@@ -1,47 +1,70 @@
-import React, { useEffect, useState } from "react";
-import logo from "../../assets/D.png";
+import { useEffect, useState } from "react";
+// import logo from "../../assets/D.png";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { TiTick } from "react-icons/ti";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 function AnimatedRegister() {
-  const onSubmit = async () => {
-    const response = await axios.post("http://localhost:4000/register", {
-      email,
-      password,
-    });
-    const { data } = await response;
-    console.log(data);
-  };
-  async function handleOnChange() {
-    const regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-    const response = await axios.post("http://localhost:4000/CheckEmail", {
-      email,
-    });
-    console.log(response.data);
-    if (response.data.success && !email.match(regex)) {
-      console.log("Email Already exists");
-      setInvalidEmail(false);
-    } else {
-      console.log("Invalid email");
-      setInvalidEmail(true);
-    }
-  }
   const [email, setEmail] = useState("");
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [emailChoosen, setEmailChoosen] = useState("");
   const [passwordChoose, setPasswordChoosen] = useState("");
   const [UserNameChoosen, setUserNameChoosen] = useState("");
+  const [regexMatched, setRegexMatched] = useState(false);
+  const [IsValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+  // const onSubmit = async () => {
+  //   const response = await axios.post("http://localhost:4000/register", {
+  //     email,
+  //     password,
+  //   });
+  //   const { data } = await response;
+  //   console.log(data.emailCheck.success);
+  // };
+  const validatePassword = () => {
+    // Regular expression for validating strong passwords
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    if (passwordRegex.test(password)) {
+      console.log(password);
+      setIsValidPassword(true);
+      console.log("Password is strong and can be used");
+    } else {
+      setIsValidEmail(false);
+    }
+  };
 
-  const [invalidEmail, setInvalidEmail] = useState(true);
+  async function ValidateEmail() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setRegexMatched(emailRegex.test(email));
+    if (regexMatched) {
+      try {
+        const response = await axios.post("http://localhost:4000/CheckEmail", {
+          email,
+        });
+        if (response.data.success) {
+          setIsValidEmail(false);
+        } else {
+          setIsValidEmail(true);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
 
   useEffect(() => {
-    handleOnChange();
-  }, [email]);
+    ValidateEmail();
+  }, [email, IsValidEmail]);
+
+  useEffect(() => {
+    validatePassword();
+    console.log(isValidPassword);
+  }, [password, isValidPassword]);
 
   return (
-    <div className="flex items-center justify-center h-[100vh] flex-col registerContainer">
+    <div className="flex items-center justify-center h-[110vh] flex-col registerContainer">
       <div className="flex fixed top-0 right-0 left-0  py-4 px-6 justify-between items-center">
         <div className="flex items-center justify-end text-white cursor-pointer">
           <span className="text-4xl">D</span>
@@ -53,22 +76,20 @@ function AnimatedRegister() {
         </Link>
       </div>
 
-      <form
-        className="p-10 flex flex-col border-2 border-[#ffffff07]  gap-5 rounded-lg w-[500px] bg-[#ffffff07]
-"
-      >
+      <div className="p-10 flex flex-col border-2 border-[#ffffff07]  gap-5 rounded-lg w-[500px] bg-[#ffffff07]">
         <h1 className="text-2xl font-bold text-slate-300">Sign up</h1>
 
-        {/* Enter your email */}
+        {/* Enter your email...... Email Field */}
 
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-2 ">
-            <label htmlFor="Email" className="text-slate-200">
+            <label htmlFor="Email" className="text-[#36a8f4]">
               Enter your Email address *
             </label>
             <div className="w-[400px] flex justify-between items-center bg-transparent gap-2">
               {!emailChoosen ? (
                 <>
+                  <FaLongArrowAltRight className="text-red-400" />
                   <input
                     type="email"
                     className="px-2 py-2 rounded-lg outline-blue-500 bg-transparent w-[80%] text-white"
@@ -80,11 +101,15 @@ function AnimatedRegister() {
                   <button
                     className={
                       "px-4 py-2 rounded-lg text-white" +
-                      (!invalidEmail
-                        ? " border-[1px] border-green-200 opacity-50 cursor-auto"
-                        : " border-[1px] border-slate-200 ")
+                      (IsValidEmail
+                        ? " border-[1px] border-green-400 opacity-100 cursor-pointer hover:bg-green-500 hover:text-black"
+                        : " border-[1px] border-slate-200 cursor-default")
                     }
-                    onClick={() => setEmailChoosen(email)}
+                    onClick={() => {
+                      if (IsValidEmail) {
+                        setEmailChoosen(email);
+                      }
+                    }}
                   >
                     Continue
                   </button>
@@ -103,7 +128,7 @@ function AnimatedRegister() {
           </div>
         </div>
 
-        {/* If email is verified then you can enter password */}
+        {/* If email is verified then you can enter password ..... Password Field */}
 
         {emailChoosen && (
           <>
@@ -125,12 +150,16 @@ function AnimatedRegister() {
                       />
                       <button
                         className={
-                          "px-4 py-2 rounded-lg text-white" +
-                          (!invalidEmail
-                            ? " border-[1px] border-green-200 opacity-50 cursor-auto"
-                            : " border-[1px] border-slate-200 ")
+                          "px-4 py-2 rounded-lg text-white font-bold" +
+                          (isValidPassword
+                            ? " border-[1px] border-green-400  cursor-pointer hover:bg-green-500 hover:text-black"
+                            : " border-[1px] border-slate-200 opacity-50 cursor-default")
                         }
-                        onClick={() => setPasswordChoosen(password)}
+                        onClick={() => {
+                          if (isValidPassword) {
+                            setPasswordChoosen(password);
+                          }
+                        }}
                       >
                         Continue
                       </button>
@@ -151,7 +180,7 @@ function AnimatedRegister() {
           </>
         )}
 
-        {/* If email and password is choosen then this will work */}
+        {/* If email and password is choosen then this will work.... User Id Field */}
 
         {passwordChoose && (
           <>
@@ -174,7 +203,7 @@ function AnimatedRegister() {
                       <button
                         className={
                           "px-4 py-2 rounded-lg text-white" +
-                          (!invalidEmail
+                          (!IsValidEmail
                             ? " border-[1px] border-green-200 opacity-50 cursor-auto"
                             : " border-[1px] border-slate-200 ")
                         }
@@ -210,7 +239,31 @@ function AnimatedRegister() {
             </div>
           </>
         )}
-      </form>
+      </div>
+      {/* Validation messages */}
+      <div className="mt-5 text-slate-400 h-30 flex justify-center items-center flex-col gap-2">
+        {IsValidEmail ? (
+          ""
+        ) : (
+          <>
+            <p className="text-center">
+              Your email is invalid or already choosen
+            </p>
+          </>
+        )}
+        {password.length <= 0 ? (
+          "Enter your password"
+        ) : isValidPassword ? (
+          ""
+        ) : (
+          <>
+            <p className="text-center">
+              password much contains least 8 characters, containing at least one
+              uppercase letter, one lowercase letter, and one digit
+            </p>
+          </>
+        )}
+      </div>
     </div>
   );
 }
