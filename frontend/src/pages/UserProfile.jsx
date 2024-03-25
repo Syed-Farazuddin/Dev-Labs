c3ab873b24a9dc28d066a08346b88f850fa3ce36
Syed Faraz <syedfarazuddin73@gmail.com>
2024-03-25 05:46:51 +0530

    profile menu has created

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
+import logo from "../assets/person.png";
 import axios from "axios";
+import { IoGlobeOutline } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import { SiCodeforces } from "react-icons/si";
+import { FaEdit } from "react-icons/fa";
 function UserProfile() {
   const [github, setGithub] = useState("");
   const [githubError, setGithubError] = useState();
   const [gitInfo, setGitInfo] = useState([]);
   const [leetcodeUser, setLeetcodeUser] = useState("");
   const [cfUser, setCfUser] = useState("");
-
   const handleSubmit = async () => {
     // https://api.github.com/users/${name}
     // https://leetcode-stats-api.herokuapp.com/<YOUR_USERNAME>
@@ -26,9 +31,62 @@ function UserProfile() {
     }
   };
   return (
-    <div className="flex justify-start items-center  py-5">
-      <div className="flex-[10%] border-2 border-red-400">Menu Bar</div>
-      <div className="flex-[80%] flex items-center gap-5 flex-col border-2 border-blue-900">
+    <div className="flex justify-start items-center p-10  bg-[#272727]  gap-10">
+      <div className="px-6 py-8 bg-[#353535] rounded-lg">
+        <div className="flex justify-start items-start p-4 rounded-lg gap-2 flex-[40%]">
+          <img className="rounded-full w-40 h-40 mr-2" src={logo} alt="" />
+          <div className="flex items-start justify-center flex-col gap-2 mt-4">
+            <div className="flex items-center justify-between gap-4 w-full">
+              <h1 className="text-2xl font-bold text-[#eeeeee] w-full">
+                Syed Farazuddin
+              </h1>
+              <div className="text-[#eeeeee] cursor-pointer">
+                <FaEdit />
+              </div>
+            </div>
+            <p className="text-slate-200 text-pretty">
+              Final Year BE Student | CSE | Web Developer | MERN | React | Java
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="flex">
+            <div className="flex items-center justify-start text-slate-200 gap-2 ml-10 mt-5 items-center">
+              <p className="text-slate-200">
+                <IoGlobeOutline />
+              </p>
+              <a href="https://syed-farazuddin.github.io/My-Portfolio/">
+                Website
+              </a>
+            </div>
+            <div className="flex items-center justify-start text-slate-200 gap-2 ml-10 mt-5">
+              <p className="text-slate-200">
+                <FaGithub />
+              </p>
+              <a href="https://syed-farazuddin.github.io/My-Portfolio/">
+                Github
+              </a>
+            </div>
+            <div className="flex items-center justify-start text-slate-200 gap-2 ml-10 mt-5">
+              <p className="text-slate-200">
+                <SiLeetcode />
+              </p>
+              <a href="https://syed-farazuddin.github.io/My-Portfolio/">
+                Leetcode
+              </a>
+            </div>
+            <div className="flex items-center justify-start text-slate-200 gap-2 ml-10 mt-5">
+              <p className="text-slate-200">
+                <SiCodeforces />
+              </p>
+              <a href="https://syed-farazuddin.github.io/My-Portfolio/">
+                Codeforces
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-6 py-8 bg-[#353535] rounded-lg w-full">
         <div className="heading">User name from This page website</div>
         <div className="userDetails">User Details of the same</div>
         <div className="github flex gap-4 items-center justify-center flex-col text-black">
