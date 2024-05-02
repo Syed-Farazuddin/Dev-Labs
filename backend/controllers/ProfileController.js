0044ef37d4dd0b7deb1a1b2516f7407ab2e9d07b
Syed Faraz <syedfarazuddin73@gmail.com>
2024-05-02 23:51:34 +0530

    backend working to update profile

--- backend/controllers/ProfileController.js
+++ backend/controllers/ProfileController.js
@@ -1,3 +1,7 @@
-const getProfile = () => {};
+const getProfile = (req, res) => {};
+
+const updateProfile = (req, res) => {
+  const user = req.header;
+};
 
 module.exports = { getProfile };
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="w-full bg-[#303030]">Footer Content`</div>;
+  return (
+    <div className="w-full loginContainer text-white flex justify-center items-center text-2xl mt-6">
+      Footer Content
+    </div>
+  );
 }
